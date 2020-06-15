c7b1d89913afe0843d60cd0f48ff14556fd452ea
lance.jiang <lance.jiang@bizfocus.cn>
2020-06-15 18:03:07 +0800

    a

---
+++ leetcode/14. Longest Common Prefix/1.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    let target = []
+    for (let i = 0; i < strs.length; i++) {
+        for (let j = 0; j < strs[i]; j++) {
+            if (i == 0) {
+                target.push(strs[i][j])
+            } else {
+                
+            }
+        }
+    }
+};
